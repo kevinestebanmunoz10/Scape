452e92032a6741af9a9ed3058ccf994ad8716d3f
Kevin Muñoz <kevinestebanmunoz10@gmail.com>
2026-09-22 11:23:01 -0500

    Consolidar columnas de codigo en password_reset_tokens dentro de create_users_table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -24,6 +24,8 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
+            $table->string('code', 255)->nullable()->after('token');
+            $table->timestamp('expires_at')->nullable()->after('code');
             $table->timestamp('created_at')->nullable();
         });
 
